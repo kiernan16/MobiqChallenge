9b19dac841fdd35a407820bcd5c68eb8e5388235
Matt Kiernan <kiernan@mail.usf.edu>
2015-03-28 10:13:50 -0400

    got file names on table

--- KiernanTest/Browse.m
+++ KiernanTest/Browse.m
@@ -5,113 +5,7 @@
 ////  Created by Matthew Kiernan on 3/28/15.
 ////  Copyright (c) 2015 Matt Kiernan. All rights reserved.
 ////
-//
-//#import <Foundation/Foundation.h>
-//#import "Browse.h"
-////#import <DropboxSDK/DropboxSDK.h>
-//
-//@interface Browse ()
-//
-//@end
-//
-//
-//@implementation Browse
-//
-//
-////    - (void)viewDidLoad {
-////        [super viewDidLoad];
-////        
-////        self->restClient = [[DBRestClient alloc] initWithSession:[DBSession sharedSession]];
-////        self->restClient.delegate = self;
-////        
-////        [self->restClient loadMetadata:@"/"];
-////    }
-//
-//- (id)initWithStyle:(UITableViewStyle)style
-//{
-//    self.tableView.delegate = self;
-//
-//    self = [super initWithStyle:style];
-//    if (self) {
-//        // Custom initialization
-//    }
-//    return self;
-//}
-//
-//
-//- (DBRestClient *)restClient {
-//    if (!restClient) {
-//        restClient =
-//        [[DBRestClient alloc] initWithSession:[DBSession sharedSession]];
-//        restClient.delegate = self;
-//    }
-//    return restClient;
-//}
-//
-//- (void)viewDidLoad
-//{
-//    self.tableView.delegate = self;
-////    self.tableView.datasource = self;
-//
-//    [super viewDidLoad];
-//    dropboxURLs = [[NSMutableArray alloc] init];
-//    [[self restClient] loadMetadata:@"/"];
-//    [self.tableView reloadData];
-//}
-//
-//
-//- (void)didReceiveMemoryWarning
-//{
-//    [super didReceiveMemoryWarning];
-//    // Dispose of any resources that can be recreated.
-//}
-//
-//- (void)restClient:(DBRestClient *)client loadedMetadata:(DBMetadata *)metadata {
-//    if (metadata.isDirectory) {
-//        NSLog(@"Folder '%@' contains:", metadata.path);
-//        for (DBMetadata *file in metadata.contents) {
-//            NSLog(@"	%@", file.filename);
-//        }
-//        [self.tableView reloadData];
-//    }
-//}
-//
-//- (void)restClient:(DBRestClient *)client
-//loadMetadataFailedWithError:(NSError *)error {
-//    NSLog(@"Error loading metadata: %@", error);
-//}
-//
-////- (void)restClient:(DBRestClient*)client loadedMetadata:(DBMetadata*)metadata {
-////    
-////    for (DBMetadata *file in metadata.contents) {
-////        NSLog(@"\t%@", file.filename);
-////        [dropBoxArray addObject:file.filename];
-////    }
-////    NSLog(@"%@", dropBoxArray);
-////    
-////    [self.tableView reloadData];
-////
-////}
-//
-//- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
-//{
-//    static NSString *CellIdentifier = @"Cell";
-//    
-//    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
-//    if (cell == nil) {
-//        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
-//    }
-//    
-//   // cell.textLabel.text = [dropBoxArray objectAtIndex:indexPath.row];
-//    NSLog(@"%@", cell.textLabel.text);
-//    
-//    return cell;
-//}
-//
-//
-//
-//@end
-//
+
 
 #import <UIKit/UIKit.h>
 #import <DropboxSDK/DropboxSDK.h>
@@ -123,9 +17,7 @@
 
 @end
 
-
-
-
+NSString *pathcomp;
 @implementation Browse
 
 - (id)initWithStyle:(UITableViewStyle)style
@@ -159,16 +51,40 @@
             if (!file.isDirectory)
             {
                 NSLog(@"%@", file.filename);
+                pathcomp = file.filename;
                 [dropboxURLs addObject:file.filename];
-                [self.tableView reloadSections:[NSIndexSet indexSetWithIndex:0] withRowAnimation:UITableViewRowAnimationFade];
+                [self DBdownload];
+                
+             //   [self.tableView reloadSections:[NSIndexSet indexSetWithIndex:0] withRowAnimation:UITableViewRowAnimationFade];
             }
         }
+    }
+    [self.tableView reloadData];
+}
+
+- (void)restClient:(DBRestClient *)client loadedFile:(NSString *)localPath
+       contentType:(NSString *)contentType metadata:(DBMetadata *)metadata {
+    NSLog(@"File loaded into path: %@", localPath);
+}
+
+-(void) DBdownload//:(id)sender
+{
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentsDirectory = [paths objectAtIndex:0]; // Get documents directory
+    NSString *filePath = [documentsDirectory stringByAppendingPathComponent:@"/"];
+    NSError *error;
+        
+    [self.restClient loadFile:@"/KiernanTest/" intoPath:filePath];
+    
+    if (filePath) { // check if file exists - if so load it:
+        NSString *tempTextOut = [NSString stringWithContentsOfFile:pathcomp
+                                                          encoding:NSUTF8StringEncoding
+                                                             error:&error];
     }
 }
 
 
-- (void)restClient:(DBRestClient *)client
-loadMetadataFailedWithError:(NSError *)error {
+- (void)restClient:(DBRestClient *)client loadMetadataFailedWithError:(NSError *)error {
     
     NSLog(@"Error loading metadata: %@", error);
 }
@@ -206,8 +122,8 @@ loadMetadataFailedWithError:(NSError *)error {
     
     cell.textLabel.text = [dropboxURLs objectAtIndex:indexPath.row];
     
-    [self.tableView reloadData];
     return cell;
 }
+
 
 @end
